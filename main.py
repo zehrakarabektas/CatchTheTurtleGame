e45aa5ed6814a2f59e10ebf433b81a30310f6618
Zehra <zehrakarabektas679@gmail.com>
2024-08-25 12:59:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ drawing_board.bgcolor("light blue")
 drawing_board.title("Catch The Turtle")
 
 score = 0
+level_cath_turtle=0
 score_display = turtle.Turtle()
 score_display.penup()
 score_display.goto(0, 250)
@@ -28,6 +29,10 @@ level_display.goto(0, 280)
 level_display.hideturtle()
 level_display.write(f"LEVEL: {1}",move=False,align="center" ,font=("Arial", 16, "normal"))
 
+message_display=turtle.Turtle()
+message_display.penup()
+message_display.hideturtle()
+
 def turtle_draw():
     x = random.randint(-220, 250)
     y = random.randint(-220, 250)
@@ -36,21 +41,28 @@ def turtle_draw():
 
 def update_score(x, y):
     global score
+    global level_cath_turtle
     if turtle_object.distance(x, y) < 20:
         score += 1
         score_display.clear()
+        score_display.goto(0, 250)
         score_display.write(f"SCORE: {score}",move=False,align="center", font=("Arial", 16, "normal"))
+        level_cath_turtle+=1
 
 def end_game(message):
     turtle_object.hideturtle()
-    score_display.goto(0, 220)
-    score_display.color("black")
+    message_display.penup()
+    message_display.goto(0, 220)
+    message_display.color("black")
+    message_display.hideturtle()
     turtle_draw()
-    score_display.write(message,move=False,align="center", font=("Arial", 16, "normal"))
+    message_display.write(message,move=False,align="center", font=("Arial", 16, "normal"))
+    drawing_board.onscreenclick(None)
 
 def game_level(message):
     turtle_object.hideturtle()
     level_display.goto(0, 280)
+    level_display.clear()
     turtle_draw()
     level_display.write(message,move=False,align="center", font=("Arial", 16, "normal"))
 drawing_board.onscreenclick(update_score)
@@ -59,6 +71,7 @@ level=5
 turtle_num=20
 turtle_speed=400
 turtle_next_speed=50
+
 for i in range(1, level + 1):
     for j in range(turtle_num):
         turtle_draw()
@@ -67,10 +80,13 @@ for i in range(1, level + 1):
     game_level(f"LEVEL: {i}")
     turtle.delay(500)
     turtle_speed-=turtle_next_speed
-    if score == 10:
+    if level_cath_turtle == 10:
         end_game("CONGRATULATIONS")
-    elif score < 10:
+        level_cath_turtle=0
+        message_display.clear()
+    elif level_cath_turtle < 10:
         end_game("GAME OVER")
+        level_cath_turtle=0
         break
 turtle.mainloop()
 
